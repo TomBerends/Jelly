8c34cd490a076c711233edf12b9449a957dc0747
Tom Berends <58705894+TilSchweiger@users.noreply.github.com>
2021-08-30 21:51:10 +0200

    Updated Documentation

--- src/main/java/com/jelly/json/JSONArray.java
+++ src/main/java/com/jelly/json/JSONArray.java
@@ -2,13 +2,41 @@ package com.jelly.json;
 
 import java.util.ArrayList;
 
-public class JSONArray {
+/**
+ * Runtime-Representation of the array type in JSON.
+ * <p>
+ * This class is implemented as an ArrayList-Delegator with additional type-checking
+ * and an enhanced {@link #toString()} method to generate a valid JSON-String to represent this JSONArray.
+ * @author Tom Berends
+ */
+public final class JSONArray {
+    /**
+     * ArrayList holding all objects in this JSONArray.
+     */
     private final ArrayList<Object> list;
 
     public JSONArray() {
         list = new ArrayList<>();
     }
 
+    /**
+     * General method to add an object to this JSONArray.
+     * <p>
+     * Note that additional type-checking is needed in order to only allow valid JSON-Types to be added.
+     * <p>
+     * For each valid JSON-Type there is a separate method that gets called by this method.
+     *
+     * @param value value to be added to this JSONArray.
+     * @throws IllegalArgumentException if {@code value} is not a valid JSON-Type.
+     * @implNote The necessary type-checking in this method can be expensive so that it should only be called when the class of {@code value} is unknown.
+     *           Otherwise the respective separate add method should be called.
+     * @see #add(boolean)
+     * @see #add(int)
+     * @see #add(float)
+     * @see #add(String)
+     * @see #add(JSONObject)
+     * @see #add(JSONArray)
+     */
     public void add(final Object value) {
         if (value == null)
             list.add(null);
@@ -28,61 +56,165 @@ public class JSONArray {
             throw new IllegalArgumentException("invalid type " + value.getClass().getSimpleName() + " for JSON value");
     }
 
+    /**
+     * Adds a boolean value to this JSONArray.
+     * @param value boolean to be added
+     */
     public void add(final boolean value) {
         list.add(value);
     }
 
+    /**
+     * Adds an integer value to this JSONArray.
+     * @param value integer to be added
+     */
     public void add(final int value) {
         list.add(value);
     }
 
+    /**
+     * Adds a float value to this JSONArray.
+     * @param value float to be added
+     */
     public void add(final float value) {
         list.add(value);
     }
 
+    /**
+     * Adds a String to this JSONArray.
+     * @param value String to be added.
+     */
     public void add(final String value) {
         list.add(value);
     }
 
+    /**
+     * Adds a JSONObject to this JSONArray.
+     * @param value JSONObject to be added.
+     * @see JSONObject
+     */
     public void add(final JSONObject value) {
         list.add(value);
     }
 
+    /**
+     * Adds a JSONArray to this JSONArray.
+     * @param value JSONArray to be added.
+     * @see JSONArray
+     */
     public void add(final JSONArray value) {
         list.add(value);
     }
 
-    public boolean getBoolean(final int idx) {
-        return (boolean) get(idx);
-    }
-
+    /**
+     * Returns the Object at the specified position in this JSONArray.
+     *
+     * @param idx position of the Object to return.
+     * @return the Object at the specified position in this JSONArray.
+     */
     public Object get(final int idx) {
         return list.get(idx);
     }
 
+    /**
+     * Returns the Object at the specified position in this JSONArray casted to a boolean value.
+     * <p>
+     * If the type of the Object at the specified position is unknown the more general {@link #get(int)} should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to a boolean.      
+     * 
+     * @param idx position of the boolean to return.
+     * @return the boolean value at the specified position.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to a boolean value.
+     * @see #get(int)
+     */
+    public boolean getBoolean(final int idx) {
+        return (boolean) get(idx);
+    }
+
+    /**
+     * Returns the Object at the specified position in this JSONArray casted to an integer value.
+     * <p>
+     * If the type of the Object at the specified position is unknown the more general {@link #get(int)} should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to an integer.      
+     *
+     * @param idx position of the integer to return.
+     * @return the integer value at the specified position.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to an integer value.
+     * @see #get(int)
+     */
     public int getInteger(final int idx) {
         return (int) get(idx);
     }
 
+    /**
+     * Returns the Object at the specified position in this JSONArray casted to a float value.
+     * <p>
+     * If the type of the Object at the specified position is unknown the more general {@link #get(int)} should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to a float.      
+     *
+     * @param idx position of the float to return.
+     * @return the float value at the specified position.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to a float value.
+     * @see #get(int)
+     */
     public float getFloat(final int idx) {
         return (float) get(idx);
     }
 
+    /**
+     * Returns the Object at the specified position in this JSONArray casted to a String.
+     * <p>
+     * If the type of the Object at the specified position is unknown the more general {@link #get(int)} should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to a String.      
+     *
+     * @param idx position of the String to return.
+     * @return the String at the specified position.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to a String.
+     * @see #get(int)
+     */
     public String getString(final int idx) {
         return (String) get(idx);
     }
 
+    /**
+     * Returns the Object at the specified position in this JSONArray casted to a JSONObject.
+     * <p>
+     * If the type of the Object at the specified position is unknown the more general {@link #get(int)} should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to a JSONObject.      
+     *
+     * @param idx position of the JSONObject to return.
+     * @return the JSONObject at the specified position.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to a JSONObject.
+     * @see #get(int)
+     * @see JSONObject
+     */
     public JSONObject getJSONObject(final int idx) {
         return (JSONObject) get(idx);
     }
 
+    /**
+     * Returns the Object at the specified position in this JSONArray casted to a JSONArray.
+     * <p>
+     * If the type of the Object at the specified position is unknown the more general {@link #get(int)} should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to a JSONArray.
+     *
+     * @param idx position of the JSONArray to return.
+     * @return the JSONArray at the specified position.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to a JSONArray.
+     * @see #get(int)
+     * @see JSONArray
+     */
     public JSONArray getJSONArray(final int idx) {
         return (JSONArray) get(idx);
     }
 
+    /**
+     * Produces a valid JSON-String describing this JSONArray.
+     *
+     * @return valid JSON-Code describing this JSONArray.
+     */
     @Override
     public String toString() {
         return list.toString();
     }
-
 }
--- src/main/java/com/jelly/json/JSONObject.java
+++ src/main/java/com/jelly/json/JSONObject.java
@@ -1,87 +1,225 @@
 package com.jelly.json;
 
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
-public class JSONObject {
-    final Map<String, Object> fields;
+/**
+ * Runtime-Representation of the object type in JSON.
+ * <p>
+ * This class is implemented as an HashMap-Delegator with additional type-checking
+ * and an enhanced {@link #toString()} method to generate a valid JSON-String to represent this JSONObject.
+ * @author Tom Berends
+ */
+public final class JSONObject {
+    /**
+     * HashMap holding all fields in this JSONObject.
+     */
+    private final HashMap<String, Object> fields;
 
     public JSONObject() {
         fields = new HashMap<>();
     }
 
+    /**
+     * General method to set a field in JSONObject.
+     * <p>
+     * Note that additional type-checking is needed in order to only allow valid JSON-Types to be set.
+     * <p>
+     * For each valid JSON-Type there is a separate method that gets called by this method.
+     *
+     * @param fieldName name of the field to be set.
+     * @param value value the field is to be set to.
+     * @throws IllegalArgumentException if {@code value} is not a valid JSON-Type.
+     * @implNote The necessary type-checking in this method can be expensive so that it should only be called when the class of {@code value} is unknown.
+     *           Otherwise the respective separate add method should be called.
+     * @see #set(String, boolean)
+     * @see #set(String, int)
+     * @see #set(String, float)
+     * @see #set(String, String)
+     * @see #set(String, JSONObject)
+     * @see #set(String, JSONArray)
+     */
     public void set(final String fieldName, final Object value) {
-        if(value == null)
+        if (value == null)
             fields.put(fieldName, null);
-        else if(value instanceof Boolean)
+        else if (value instanceof Boolean)
             set(fieldName, (boolean) value);
-        else if(value instanceof Integer)
+        else if (value instanceof Integer)
             set(fieldName, (int) value);
-        else if(value instanceof Float)
+        else if (value instanceof Float)
             set(fieldName, (float) value);
-        else if(value instanceof final String stringValue)
+        else if (value instanceof final String stringValue)
             set(fieldName, stringValue);
-        else if(value instanceof final JSONObject jsonObject)
+        else if (value instanceof final JSONObject jsonObject)
             set(fieldName, jsonObject);
-        else if(value instanceof final JSONArray jsonArray)
+        else if (value instanceof final JSONArray jsonArray)
             set(fieldName, jsonArray);
         else
             throw new IllegalArgumentException("invalid type " + value.getClass().getSimpleName() + " for JSON value");
     }
 
+    /**
+     * Sets the Field in this JSONObject with the specified name to a boolean value.
+     * @param fieldName name of the field to set.
+     * @param value value the field is to be set to.
+     */
     public void set(final String fieldName, final boolean value) {
-        fields.put(Objects.requireNonNull(fieldName), value);
+        fields.put(fieldName, value);
     }
 
+    /**
+     * Sets the Field in this JSONObject with the specified name to an integer value.
+     * @param fieldName name of the field to set.
+     * @param value value the field is to be set to.
+     */
     public void set(final String fieldName, final int value) {
-        fields.put(Objects.requireNonNull(fieldName), value);
+        fields.put(fieldName, value);
     }
 
+    /**
+     * Sets the Field in this JSONObject with the specified name to a float value.
+     * @param fieldName name of the field to set.
+     * @param value value the field is to be set to.
+     */
     public void set(final String fieldName, final float value) {
-        fields.put(Objects.requireNonNull(fieldName), value);
+        fields.put(fieldName, value);
     }
 
+    /**
+     * Sets the Field in this JSONObject with the specified name to a String.
+     * @param fieldName name of the field to set.
+     * @param value value the field is to be set to..
+     */
     public void set(final String fieldName, final String value) {
-        fields.put(Objects.requireNonNull(fieldName), value);
+        fields.put(fieldName, value);
     }
 
+    /**
+     * Sets the Field in this JSONObject with the specified name to a JSONObject.
+     * @param fieldName name of the field to set.
+     * @param value value the field is to be set to..
+     * @see JSONObject
+     */
     public void set(final String fieldName, final JSONObject value) {
-        fields.put(Objects.requireNonNull(fieldName), value);
+        fields.put(fieldName, value);
     }
 
+    /**
+     * Sets the Field in this JSONObject with the specified name to a JSONArray.
+     * @param fieldName name of the field to set.
+     * @param value value the field is to be set to..
+     * @see JSONArray
+     */
     public void set(final String fieldName, final JSONArray value) {
-        fields.put(Objects.requireNonNull(fieldName), value);
+        fields.put(fieldName, value);
     }
 
+    /**
+     * Returns the value of the field with the specified name.
+     *
+     * @param fieldName name of the field.
+     * @return the value of the field with the specified name.
+     */
     public Object get(final String fieldName) {
         return fields.get(fieldName);
     }
 
+    /**
+     * Returns the value of the field with the specified name cast to a boolean value.
+     * <p>
+     * If the type of the value of the specified field is unknown the more general {@link #get(String)} method should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to a boolean value.
+     *
+     * @param fieldName the name of the field.
+     * @return the value of the field with the specified name.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to a boolean value.
+     * @see #get(String)
+     */
     public boolean getBoolean(final String fieldName) {
         return (boolean) get(fieldName);
     }
 
+    /**
+     * Returns the value of the field with the specified name cast to an integer value.
+     * <p>
+     * If the type of the value of the specified field is unknown the more general {@link #get(String)} method should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to an integer value.
+     *
+     * @param fieldName the name of the field.
+     * @return the value of the field with the specified name.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to an integer value.
+     * @see #get(String)
+     */
     public int getInteger(final String fieldName) {
         return (int) get(fieldName);
     }
 
+    /**
+     * Returns the value of the field with the specified name cast to a float value.
+     * <p>
+     * If the type of the value of the specified field is unknown the more general {@link #get(String)} method should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to a float value.
+     *
+     * @param fieldName the name of the field.
+     * @return the value of the field with the specified name.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to a float value.
+     * @see #get(String)
+     */
     public float getFloat(final String fieldName) {
         return (float) get(fieldName);
     }
 
+    /**
+     * Returns the value of the field with the specified name cast to a String.
+     * <p>
+     * If the type of the value of the specified field is unknown the more general {@link #get(String)} method should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to a String.
+     *
+     * @param fieldName the name of the field.
+     * @return the value of the field with the specified name.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to a String.
+     * @see #get(String)
+     */
     public String getString(final String fieldName) {
         return (String) get(fieldName);
     }
 
+    /**
+     * Returns the value of the field with the specified name cast to a JSONObject.
+     * <p>
+     * If the type of the value of the specified field is unknown the more general {@link #get(String)} method should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to a JSONObject.
+     *
+     * @param fieldName the name of the field.
+     * @return the value of the field with the specified name.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to a JSONObject.
+     * @see #get(String)
+     * @see JSONObject
+     */
     public JSONObject getJSONObject(final String fieldName) {
         return (JSONObject) get(fieldName);
     }
 
+    /**
+     * Returns the value of the field with the specified name cast to a JSONArray.
+     * <p>
+     * If the type of the value of the specified field is unknown the more general {@link #get(String)} method should be called
+     * as this method throws an Exception when the Object at the specified position cannot be cast to a JSONArray.
+     *
+     * @param fieldName the name of the field.
+     * @return the value of the field with the specified name.
+     * @throws ClassCastException if the Object at the specified position cannot be cast to a JSONArray.
+     * @see #get(String)
+     * @see JSONArray
+     */
     public JSONArray getJSONArray(final String fieldName) {
         return (JSONArray) get(fieldName);
     }
 
+    /**
+     * Produces a valid JSON-String describing this JSONObject.
+     *
+     * @return valid JSON-Code describing this JSONObject.
+     */
     @Override
     public String toString() {
         return fields.toString();
